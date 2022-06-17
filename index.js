b500f6a1484aa5d22ca43bb7dabfa8ab25d4e82a
Abdul Munim <[abdulmunim.007.pk@gmail.com]>
2022-06-18 00:18:01 +0100

    js bg color changer

---
+++ index.js
@@ -0,0 +1,14 @@
+const getHexCode = () => {
+  let hexBits = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += hexBits[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+function changeColor() {
+  const hexCode = getHexCode()
+  document.getElementById('hex').innerHTML = hexCode
+  document.getElementsByTagName('BODY')[0].style.backgroundColor = hexCode
+}
